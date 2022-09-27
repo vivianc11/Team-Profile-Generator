ace01b3c2f9505302947154bf8f5527cd2b642f9
Vivian Chen <vivian.chen1193@gmail.com>
2022-09-26 21:09:35 -0700

    created prompt for employees, intern, and engineer roles

--- index.js
+++ index.js
@@ -60,5 +60,65 @@ const questions = () => {
 }
 
 const moreQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'What role do you want to assign the employee?',
+            choices: [
+                'Intern',
+                'Engineer'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'employeeName',
+            message: "What's the employee's name?",
+            validate: (response) => {
+                return validation.required(response);
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: "What's the employee's ID?",
+            validate: (response) => {
+                return validation.required(response);
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeEmail',
+            message: "What's the employee's Email?",
+            validate: (response) => {
+                return validation.required(response);
+            }
+        }
+    ])
+    .then((response) => {
+        if (response.employeeRole === 'Intern') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'internSchool',
+                    message: 'What school is the Intern from?',
+                    validate: (response) => {
+                        return validation.required(response);
+                    }
+                }
+            ])
 
+        } else if (response.employeeRole === 'Engineer') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'engineerGithub',
+                    message: "What is the engineer's GitHub username?",
+                    validate: (response) => {
+                        return validation.required(response);
+                    }
+                }
+            ])
+        }
+    } )
 }
