306d895e9479bfd7a85a797848ff23540c28f371
Vivian Chen <vivian.chen1193@gmail.com>
2022-09-27 15:33:18 -0700

    corrected test for manager so it now passes

--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -3,7 +3,7 @@ const Manager = require('../lib/manager');
 test ('Should create a manager', () => {
     const e = new Manager('managerName', 1, 'managerEmail', 2);
     expect(e.name).toBe('managerName');
-    expect(e.id).toBe('1');
+    expect(e.id).toBe(1);
     expect(e.email).toBe('managerEmail');
     expect(e.officeNumber).toBe(2);
 })
